94602cd4a03bc7e5afe3eb1a350ae69ceda291f1
Mateusz Suplewski <matx3582@gmail.com>
2022-07-17 18:34:59 +0200

    add load excursions to admin page

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,5 +1,30 @@
-import './../css/admin.css';
+import './../css/admin.css'
+import ExcursionsAPI from './ExcursionsAPI'
+import ExcursionsLoader from './ExcursionsLoader'
 
-import ExcursionsAPI from './ExcursionsAPI';
+const api = new ExcursionsAPI('http://localhost:3000/excursions')
 
-console.log('admin');
+const init = () => {
+    load()
+
+}
+
+document.addEventListener('DOMContentLoaded', init)
+
+const load = () => {
+    api.loadData()
+        .then(data => insert(data))
+        .catch(error => console.log("error", error))
+}
+
+const insert = (excursionsList) => {
+    const excursionsPanelEl = findExcursionsPanel()
+    const excursionPrototypeEl = excursionsPanelEl.querySelector('.excursions__item--prototype')
+    const loader = new ExcursionsLoader(excursionsPanelEl, excursionsList, excursionPrototypeEl)
+    loader.init()
+}
+
+const findExcursionsPanel = () => {
+    return document.querySelector('.panel__excursions')
+}
+
