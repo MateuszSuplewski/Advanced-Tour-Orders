5ecfe9cdef08ff3814ab57107ddece7c6dbbc117
Mateusz Suplewski <matx3582@gmail.com>
2022-07-17 18:37:07 +0200

    add admin functionality - add excursion

--- src/js/admin.js
+++ src/js/admin.js
@@ -6,7 +6,7 @@ const api = new ExcursionsAPI('http://localhost:3000/excursions')
 
 const init = () => {
     load()
-
+    add()
 }
 
 document.addEventListener('DOMContentLoaded', init)
@@ -24,7 +24,32 @@ const insert = (excursionsList) => {
     loader.init()
 }
 
+const add = () => {
+    const formEl = document.querySelector('.form')
+
+    formEl.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const data = getFormData(e.target)
+
+        api.addData(data)
+            .catch(error => console.log("error", error))
+            .finally(() => load())
+    })
+}
+
+
+
 const findExcursionsPanel = () => {
     return document.querySelector('.panel__excursions')
 }
 
+const getFormData = (form) => {
+    const formElements = form.elements
+    const { title, description, adultPrice, childrenPrice } = formElements
+    return {
+        title: title.value,
+        description: description.value,
+        adultPrice: adultPrice.value,
+        childrenPrice: childrenPrice.value
+    }
+}
